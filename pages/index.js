c60b7bb47e98c8675f04df33ed7a974b9924d2eb
Humberto Piaia <betopiaia@gmail.com>
2020-11-14 21:39:53 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>oddipattern.art</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>oddipattern.art</p>
+    </div>
+  )
+}
