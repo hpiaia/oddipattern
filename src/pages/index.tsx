b39d578ebc85da488e0ebeb73ac207a6deb21420
Humberto Piaia <betopiaia@gmail.com>
2020-11-26 22:50:09 -0300

    create landing page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="h-full bg-black flex items-center justify-center">
+      <div className="text-white text-center text-7xl barcode39">
+        <p>COMING</p>
+        <p>SOON</p>
+      </div>
+    </div>
+  )
+}
