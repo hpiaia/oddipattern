6519f95776046e7e5a2e9d9bfa90fbb444d40352
Humberto Piaia <betopiaia@gmail.com>
2020-11-26 22:56:10 -0300

    add pwa support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="oddipattern.art" />
+        <meta name="keywords" content="oddi, pattern, art" />
+        <meta name="theme-color" content="#000" />
+
         <title>oddipattern.art</title>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
